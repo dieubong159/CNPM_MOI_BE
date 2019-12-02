895510b5981e3026fb6f2f738f74f943198b0bdc
dieubong159 <dieubong159@gmail.com>
2019-12-02 21:23:52 +0700

    fb change client id

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
   secret: "Anonymous.Jaxkie vs gao_com_zero",
   oauth: {
     facebook: {
-      clientID: "728173534347534",
-      clientSecret: "50d26afdc5031b78d766cc20929dae46"
+      clientID: "1011815245824793",
+      clientSecret: "1f58b2f21f00088181d4d1491b76855d"
     }
   }
 };

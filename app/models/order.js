0a1556b1cee749a01b8988bd8e7fb56c88e60ceb
dieubong159 <dieubong159@gmail.com>
2019-12-04 00:02:52 +0700

    fix time email

--- app/models/order.js
+++ app/models/order.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 var ProductModel = require("./product");
 var ProductSchema = ProductModel.ProductSchema;
 var nodemailer = require("nodemailer");
+var moment = require("moment");
 
 var transporter = nodemailer.createTransport({
   service: "gmail",
@@ -85,10 +86,10 @@ exports.confirmOrder = (orderId, orderData) => {
             '<!doctype html><html><head> <meta charset="utf-8"><style> .invoice-box { max-width: 800px; margin: auto; padding: 30px; border: 1px solid #eee; box-shadow: 0 0 10px rgba(0, 0, 0, .15); font-size: 16px; line-height: 24px; font-family: \'Helvetica Neue\', \'Helvetica\', Helvetica, Arial, sans-serif; color: #555; } .invoice-box table { width: 100%; line-height: inherit; text-align: left; } .invoice-box table td { padding: 5px; vertical-align: top; } .invoice-box table tr td:nth-child(2) { text-align: right; } .invoice-box table tr.top table td { padding-bottom: 20px; } .invoice-box table tr.top table td.title { font-size: 45px; line-height: 45px; color: #333; } .invoice-box table tr.information table td { padding-bottom: 40px; } .invoice-box table tr.heading td { background: #eee; border-bottom: 1px solid #ddd; font-weight: bold; } .invoice-box table tr.details td { padding-bottom: 20px; } .invoice-box table tr.item td{ border-bottom: 1px solid #eee; } .invoice-box table tr.item.last td { border-bottom: none; } .invoice-box table tr.total td:nth-child(2) { border-top: 2px solid #eee; font-weight: bold; } @media only screen and (max-width: 600px) { .invoice-box table tr.top table td { width: 100%; display: block; text-align: center; } .invoice-box table tr.information table td { width: 100%; display: block; text-align: center; } } /** RTL **/ .rtl { direction: rtl; font-family: Tahoma, \'Helvetica Neue\', \'Helvetica\', Helvetica, Arial, sans-serif; } .rtl table { text-align: right; } .rtl table tr td:nth-child(2) { text-align: left; } </style></head><body> <div class="invoice-box"> <table cellpadding="0" cellspacing="0"> <tr class="top"> <td colspan="2"> <table> <tr> <td class="title"> Đơn hàng của bạn đã được xác nhận và sẽ được chuyển trong vài ngày tới <3 </td> <td> Mã hóa đơn: #' +
             order._id +
             "<br> Ngày tạo:" +
-            Date.now() +
+            moment().format("DD-MM-YYYY") +
             '<br> </td> </tr> </table> </td> </tr> <tr class="information"> <td colspan="2"> <table> <tr> <td> Tiêu dân Seafood.<br> Số 1 Võ Văn Ngân<br> quận Thủ Đức, Tp. Hồ Chí Minh </td> <td> Nhóm 19.<br> Phạm Ngọc Diêu<br> ngocdieupham711@gmail.com </td> </tr> </table> </td> </tr> <tr class="heading"> <td> Phương thức thanh toán </td> <td> Phí vận chuyển </td> </tr> <tr class="details"> <td> ' +
             (order.paymentMethod ? "Tiền mặt" : "Chuyển khoản") +
-            '</td> <td> 30.000 VNĐ </td> </tr> <tr class="heading"> <td> Vật phẩm </td> <td> Giá </td> </tr>' +
+            '</td> <td> Miễn phí </td> </tr> <tr class="heading"> <td> Vật phẩm </td> <td> Giá </td> </tr>' +
             HtmlItemsForm +
             '<tr class="total"> <td></td> <td> Tổng: ' +
             HtmlTotalPriceForm +
